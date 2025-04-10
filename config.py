a41d48730aa14dd2d8b6d2e6c471dff27fd11e28
ZeaF9557 <avilashgairola@gmail.com>
2025-04-11 02:54:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = os.environ.get("API_ID", "29798096")
+API_ID = os.environ.get("API_ID", "28773814")
 
-API_HASH = os.environ.get("API_HASH", "4e8880f376d170836031cf0d97e7d55e")
+API_HASH = os.environ.get("API_HASH", "9f3a3bf87c29aa6d407f00b841e8950c")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8029033027:AAHjimy0P_NOnLYAM3FHr0yvcx76pGC2o2Q")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7934834625:AAGBF2bF9NjMIi8onxjMJRpmkqiV8B3LKyk")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER",1636484639 ))
+#OWNER = int(os.environ.get("OWNER",6156388588 ))
 
 #LOG = -1002159628443,
 
@@ -17,8 +17,8 @@ PASS_DB = int(os.environ.get("PASS_DB", "721"))
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
-    #for x in (os.environ.get("ADMINS", "7827463899").split()):
+    #ADMINS=[6156388588]
+    #for x in (os.environ.get("ADMINS", "6156388588").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
